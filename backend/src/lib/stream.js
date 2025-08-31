3fead216821f833562e4f2b476c713fb8d22a3fc
gayu999Code <gayatri9mohite@gmail.com>
2025-08-31 18:28:31 +0530

    added the stream user functionality

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,23 +1,20 @@
-import {StreamChat} from "stream-chat";
+import { StreamChat } from "stream-chat";
 import "dotenv/config";
 
 const apiKey = process.env.STREAM_API_KEY;
-const secretKey = process.env.STREAM_API_SECRET;
+const apiSecret = process.env.STREAM_API_SECRET;
 
-if(!apiKey || !secretKey){
-    console.error("STREAM API KEY and STREAM API SECRET is missing");
+if (!apiKey || !apiSecret) {
+  console.error("Stream API key or Secret is missing");
 }
-
-const streamClient = StreamChat.getInstance(apiKey,secretKey);
-
-export const upsertStreamUser = async (userData) =>{
-    try{
-        await streamClient.upsertUsers([userData]);
-        return userData;
-    }
-    catch(error){
-        console.error("Error upserting stream user",error);
-    }
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+export const upsertStreamUser = async (userData) => {
+  try {
+    await streamClient.upsertUsers([userData]);
+    return userData;
+  } catch (error) {
+    console.error("Error upserting Stream user:", error);
+  }
 };
 
 // to do it 
