28b6845d589ef7d900820a964483da0458d1a1e3
gayu999Code <gayatri9mohite@gmail.com>
2025-09-01 16:32:43 +0530

    added the route for onboarding

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { signup , login , logout } from '../controllers/auth.controller.js';
+import { protectRoute } from "../middleware/auth.middleware.js"; 
+import { onboard } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +10,12 @@ router.post('/signup',signup);
 router.post("/login",login);
 
 router.post("/logout",logout);
+
+router.post("/onboarding", protectRoute, onboard);
+
+// to check if user is logged in or not 
+router.get("/me",protectRoute,(req,res)=>{
+    res.status(200).json({success:true,user:req.user});
+});
 
 export default router;
